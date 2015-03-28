9ce4a3a2e86c868caa660d9fa4d97795eed924a8
msrxadrez <msrxadrez@users.noreply.github.com>
2015-03-28 18:41:53 -0300

    Update Hiperlink.java

--- Hiperlink.java
+++ Hiperlink.java
@@ -11,7 +11,7 @@ public class Hiperlink {
 	
 	public void editName(){
 		Scanner x = new Scanner(System.in);
-		System.out.println("Digite o nome do Hiperlink: ");	
+		System.out.println("Digite o novo nome do Hiperlink: ");	
 		this.name = x.nextLine();
 		this.modificationDate = new Date();
 	}
@@ -22,7 +22,7 @@ public class Hiperlink {
 
 	public void editUrl(){
 		Scanner x = new Scanner(System.in);
-		System.out.println("Digite a url do Hiperlink: ");	
+		System.out.println("Digite a nova url do Hiperlink: ");	
 		this.url =  x.nextLine();
 		this.modificationDate = new Date();
 	}
@@ -33,7 +33,7 @@ public class Hiperlink {
 	
 	public void editComment(){
 		Scanner x = new Scanner(System.in);
-		System.out.println("Digite um comentario a respeito do Hiperlink: ");	
+		System.out.println("Digite um novo comentario a respeito do Hiperlink: ");	
 		this.comment =  x.nextLine();
 		this.modificationDate = new Date();
 	}
@@ -68,6 +68,12 @@ public class Hiperlink {
 		System.out.println(this.modificationDate.toString());
 	}
 	
+	Hiperlink(){
+		creationDate = new Date();
+		metatags = new ArrayList <String>();
+		modificationDate = creationDate;
+	}
+	
 	Hiperlink(String newName, String newUrl){
 		name = newName;
 		url = newUrl;
@@ -75,36 +81,5 @@ public class Hiperlink {
 		metatags = new ArrayList <String>();
 		modificationDate = creationDate;
 	}
-	
-	public static void main(String[] args) {
-		Hiperlink h = new Hiperlink("teste", "teste");
-		int opt;
-		Scanner x = new Scanner(System.in);
-		for(;;){
-			System.out.println("O que deseja fazer? 1.editName 2.getName 3.editUrl 4.getUrl 5.editComment 6.getComment 7.addMetatags 8.removeMetatags 9.getMetatags 10.getCreationDate 11.getModificationDate");
-			opt = x.nextInt();
-			if(opt == 1)
-				h.editName();
-			else if(opt == 2)
-				h.getName();
-			else if(opt == 3)
-				h.editUrl();
-			else if(opt == 4)
-				h.getUrl();
-			else if(opt == 5)
-				h.editComment();
-			else if(opt == 6)
-				h.getComment();
-			else if(opt ==7)
-				h.addMetatag();
-			else if(opt == 8)
-				h.removeMetatag();
-			else if(opt == 9)
-				h.getMetatags();
-			else if(opt == 10)
-				h.getCreationDate();
-			else if(opt == 11)
-				h.getModificationDate();
-		}
-	}
+
 }
